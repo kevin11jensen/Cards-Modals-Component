d9832a362b90aa66287ec835fe3d14a115a4e513
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-23 13:52:21 -0700

    modals positioned correctcly on desktop view

--- src/App.js
+++ src/App.js
@@ -59,13 +59,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal1
-            <button onClick = {() => {document.getElementById('modal1').style.display = 'none'}} class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal1').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
-            Lorem ipsum dolor, sit amet consectetur ajss.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal1').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal1').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -76,13 +76,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal2
-            <button onClick = {() => {document.getElementById('modal2').style.display = 'none'}}class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal2').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
             Lorem ipsum dolor, sit amet consectetur ajss.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal2').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal2').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -93,13 +93,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal3
-            <button onClick = {() => {document.getElementById('modal3').style.display = 'none'}}class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal3').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
             Lorem ipsum dolor, sit amet consectetur ajss.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal3').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal3').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -111,13 +111,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal4
-            <button onClick = {() => {document.getElementById('modal4').style.display = 'none'}} class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal4').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
             Lorem ipsum dolor, sit amet consectetur ajss.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal4').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal4').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -128,13 +128,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal5
-            <button onClick = {() => {document.getElementById('modal5').style.display = 'none'}}class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal5').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
             Lorem ipsum dolor, sit amet consectetur ajss.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal5').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal5').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -145,13 +145,13 @@ function App() {
         <div className = 'modal-content'>
           <div className = 'modal-header'>
             Modal6
-            <button onClick = {() => {document.getElementById('modal6').style.display = 'none'}}class = 'modal-close'>X</button>
+            <button onClick = {() => {document.getElementById('modal6').style.display = 'none'}} className = 'modal-close'>X</button>
           </div>
           <div className = 'modal-body'>
             Lorem ipsum dolor, sit amet consectetur ajss.
           </div>
           <div className = 'modal-footer'>
-            <button onClick = {() => {document.getElementById('modal6').style.display = 'none'}} class = 'link modal-close'>
+            <button onClick = {() => {document.getElementById('modal6').style.display = 'none'}} className = 'modal-close'>
               Close
             </button>
           </div>
@@ -166,24 +166,41 @@ export default App;
 
 const ModalWrapper = styled.div `
   background: lightgray;
+  height: 100vh;
   .cards-container-top {
     display: flex;
     justify-content: center;
+    @media (max-width: 425px) {
+      flex-direction: column;
+      align-items: center;
+    }
     .card {
       text-align: center;
       width: 25%;
       margin: 1%;
       background: white;
+      height: 20rem;
+      @media (max-width: 425px) {
+        width: 95%;
+      }
     }
   }
   .cards-container-bottom {
     display: flex;
     justify-content: center;
+    @media (max-width: 425px) {
+      flex-direction: column;
+      align-items: center;
+    }
     .card {
       text-align: center;
       width: 25%;
       margin: 1%;
       background: white;
+      height: 20rem;
+      @media (max-width: 425px) {
+        width: 95%;
+      }
     }
   }
   .modal-open {
@@ -202,16 +219,27 @@ const ModalWrapper = styled.div `
     left: 0;
     background: rgba(0, 0, 0, 0.5);
     display: none;
-
+    
     animation: modal-open .5s;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   .modal-content {
     background: white;
-    margin: 10% auto;
+    margin: 3% auto;
+    height: 66vh;
     width: 50%;
+    overflow: auto;
     border-radius: .5rem;
     z-index: 4;
     padding: 3%;
+    
+    @media (max-width: 425px) {
+      width: 99%;
+      height: 99vh;
+      margin: 0;
+    }
   }
   .modal-header {
     font-weight: 500;
